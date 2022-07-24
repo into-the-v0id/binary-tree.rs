94aeaad0f3123344d4af6fe194e99ef3a76d6bcf
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-07-24 15:15:02 +0200

    Changed package type to library

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// TODO: delete item in tree
+// TODO: use into/into_iterator traits
+// TODO: optimize/order tree
+
+#![allow(dead_code)]
+
+pub use tree::BinaryTree;
+pub use node::Node;
+
+mod tree;
+mod node;
