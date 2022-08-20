add55c21fd6e23a24f3c3500d6926ec510287a40
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-08-20 11:58:03 +0200

    Added license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright Oliver Amann
+//
+// Licensed under the MIT License or the Apache License Version 2.0,
+// at your option. You may not use this file except according to
+// those terms.
+
 // TODO: delete item in tree
 // TODO: use into/into_iterator traits
 // TODO: optimize/order tree
